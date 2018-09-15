4d763ca7b56e9c4ee89b6c8cccf5f18a064c49be
satrianera <42956775+satrianera@users.noreply.github.com>
2018-09-16 04:43:40 +0700

    Add files via upload

--- safelinkv2.js
+++ safelinkv2.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="09/article1.html"}
 if(x=="2"){xxx="09/article2.html"}
 if(x=="3"){xxx="09/article3.html"}
-if(x=="4"){xxx="09/article4.html"}a_to_ve[i].href="https://neranovel.me/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="09/article4.html"}a_to_ve[i].href="https://www.neranovel.me/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
